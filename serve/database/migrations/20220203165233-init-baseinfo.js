de66d1ade033fa0fb1390e4b4b9e01d057ce8802
zxlfly <zxlfly@foxmail.com>
2022-02-04 01:08:27 +0800

    feat: 新增两张表

---
+++ serve/database/migrations/20220203165233-init-baseinfo.js
@@ -0,0 +1,25 @@
+'use strict';
+module.exports = {
+  // 在执行数据库升级时调用的函数，创建 baseinfo 表
+  up: async (queryInterface, Sequelize) => {
+    const { INTEGER, DATE, STRING ,DataTypes} = Sequelize;
+    await queryInterface.createTable('baseinfo', {
+      id: { type: INTEGER, primaryKey: true,allowNull: false },
+      juejin:STRING,
+      github:STRING,
+      gitee:STRING,
+      yuque:STRING,
+      weixin:STRING,
+      qq:STRING,
+      des:STRING,
+      logo_white:STRING,
+      logo_black:STRING,
+      created_at: DATE,
+      updated_at: DATE,
+    });
+  },
+  // 在执行数据库降级时调用的函数，删除 baseinfo 表
+  down: async queryInterface => {
+    await queryInterface.dropTable('baseinfo');
+  },
+};
